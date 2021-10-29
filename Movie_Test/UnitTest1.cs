7ea7a41750c946f2f0c87624ac7effbaaa2b6e17
Lynn Simonen <lynnsimonen@yahoo.com>
2021-10-28 21:47:09 -0500

    started adding test

---
+++ Movie_Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace Movie_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            Movie movie = new Movie();
+
+            //Act
+
+            //Assert
+        }
+    }
+}
